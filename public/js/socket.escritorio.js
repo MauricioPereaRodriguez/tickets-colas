ddbd9765cb7c0bc24f014d94d7f391382dfe8bfb
Mauricio Perea Rodríguez <mperea@portalteck.com>
2018-06-03 12:53:07 -0500

    Primer commit

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,29 @@
+// Comando para establecer la comunicación
+var socket = io();
+var label = $('#ticket');
+var searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+}
+
+var escritorio = searchParams.get('escritorio');
+var label = $('small');
+
+console.log(escritorio);
+$('h1').text('Escritorio ' + escritorio);
+
+$('button').on('click', function() {
+
+    socket.emit('atenderTicket', { escritorio: escritorio }, function(resp) {
+
+        if (resp === 'No hay tickets') {
+            alert(resp);
+            label.text(resp);
+            return;
+        }
+
+        label.text(resp.numero);
+    });
+});
